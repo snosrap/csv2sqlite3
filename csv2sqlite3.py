5b66a795157a9ebaac45fda027327f5f74f8a065
Ford <github@snosrap.com>
2013-01-26 13:04:46 -0600

    Update csv2sqlite3.py
    Added guess_datatypes method

--- csv2sqlite3.py
+++ csv2sqlite3.py
@@ -32,19 +32,52 @@ def convert(csvpath, dbpath=None, table=None):
 		if has_header:
 			r.next()
 
+		fieldtypes = guess_datatypes(r)
+
+		f.seek(0)
+		if has_header:
+			r.next()
+
 		# connect to database
 		with sqlite3.connect(dbpath) as conn:
 			conn.text_factory = str
 			c = conn.cursor()
 
 			# conditional create
-			sql_create = 'CREATE TABLE IF NOT EXISTS `%s` (%s);' % (table, ','.join(fieldnames))
+			sql_create = 'CREATE TABLE IF NOT EXISTS `%s` (%s);' % (table, ','.join(['`%s` %s' % (n, t) for (n, t) in zip(fieldnames, fieldtypes)]))
 			c.execute(sql_create)
 
 			# insert csv values
 			sql_insert = 'INSERT INTO `%s` VALUES (%s);' % (table, ','.join(['?']*len(fieldnames)))
 			for row in r:
 				c.execute(sql_insert, row)
+
+def guess_datatypes(csvreader, max=100):
+	types = []
+	for i, row in enumerate(csvreader):
+		if len(types) == 0:
+			types.extend([None] * len(row))
+		if i >= max:
+			break
+		for j, cell in enumerate(row):
+			if types[j] == None:
+				for type_try in [long, float]:
+					try:
+						type_try(cell)
+						types[j] = type_try
+						break
+					except:
+						pass
+			elif types[j] in [long, float]:
+				try:
+					types[j](cell)
+				except:
+					types[j] = str
+
+	conversion = {long:"INTEGER",float:"REAL",str:"TEXT",None:"TEXT"}
+	
+	return [conversion[x] for x in types]
+
 
 if __name__ == '__main__':
 	if len(sys.argv) <= 1:
